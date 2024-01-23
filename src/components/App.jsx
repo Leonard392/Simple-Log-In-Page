6bfe3fcd0a4e90e0e2796cdc2ef703820b368f52
Leonard392 <leorp392@gmail.com>
2024-01-23 12:44:01 -0500

    components Input and Login created

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./Login";
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
